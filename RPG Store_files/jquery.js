8c50f951109de2a2d02bfdb9101a3ea98802bed7
Henrique Liberato <henriquebmx@gmail.com>
2017-01-13 17:20:19 -0200

    Commit inicial

---
+++ RPG Store_files/jquery.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>DA8A64FD2CFC65F0</RequestId><HostId>J2/T+CRYzuvOqZuEYkatGlZj0vxsZ7XW4GDWHxI7rv/IaMLVUxPy8QxCnFJq8fYnzKx4RYGbgtk=</HostId></Error>
